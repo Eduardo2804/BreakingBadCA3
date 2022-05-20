3ed57a8a7f36792d85a0e01d4876ad32f2635dae
Carlos Eduardo Carmo do Nascimento <karloseduardo19@hotmail.com>
2022-05-20 00:53:02 +0000

    deaths changing

--- src/app/pages/deaths/deaths.page.ts
+++ src/app/pages/deaths/deaths.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-deaths',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsPage implements OnInit {
 
-  constructor() { }
+  deaths: Observable <any>;
 
+
+  constructor(private  activatedRoute: Route, private api: ApiService) { }
+
+ 
   ngOnInit() {
-  }
+    this.deaths = this.api.getDeaths();
+    this.deaths.subscribe(data => {
+    console.log('my data: ', data);
+  });
+}
 
+openDetails(death){
+    let deathId = death_id;
+    this.activatedRoute.navigateByUrl(`/tabs/deaths/${deathId}`);
+}
 }
